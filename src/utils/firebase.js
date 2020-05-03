f605bd5c59e3d200b2f190f6f18378652d8d3b36
Leandher <leandhersantos@gmail.com>
2020-05-03 17:59:50 -0300

    (chore) configure firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyCU1GaRMvpvHNKf2nKgj5fQ2WkVzdPiDOE',
+  authDomain: 'time-tracking-798b8.firebaseapp.com',
+  databaseURL: 'https://time-tracking-798b8.firebaseio.com',
+  projectId: 'time-tracking-798b8',
+  storageBucket: 'time-tracking-798b8.appspot.com',
+  messagingSenderId: '162591039220',
+  appId: '1:162591039220:web:8ec29008d0c2a1e62888a1',
+  measurementId: 'G-8ZX6CR5808',
+};
+
+export const firebaseImpl = firebase.initializeApp(config);
+
+export const currentUser = firebase.auth().currentUser;
+
+export const firebaseDatabase = firebase.database();
